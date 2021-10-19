f8fba0aae4640f60ea9a9ab6e0fda020feac2864
Kaja04001 <92801249+Kaja04001@users.noreply.github.com>
2021-10-19 15:44:25 +0200

    Add files via upload

---
+++ Slumpen/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Slumpen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int slump = rnd.Next(1, 101);
+            int FailCounter = 0;
+            Console.WriteLine("Ange ett heltal");
+            while(true)
+            {
+                int answer = int.Parse(Console.ReadLine());
+
+                if(answer == slump)
+                {
+                    Console.WriteLine("Du gissade rätt! Det tog: " + FailCounter + " försök!");
+                    break;
+                }
+                else if(answer > slump)
+                {
+                    Console.WriteLine("Du gissade för högt!");
+                    FailCounter++;
+                }
+                else if(answer < slump)
+                {
+                    Console.WriteLine("Du gissade för lågt!");
+                    FailCounter++;
+                }
+            }
+        }
+    }
+}
